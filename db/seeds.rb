1f767196536d4f985cc4b7f21c96cc6b333be12f
aydar0894 <aydar0894@mail.ru>
2018-02-08 03:36:13 +0300

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+#Создание доктора
+doc = User.create(
+	email: 'doctor1@mail.ru',
+	password: '123321',
+	role: :doctor
+	)
+
+Doctor.create(
+	first_name: 'Николай',
+	last_name: 'Иванов',
+	middle_name: 'Сергеевич',
+	birth_date: '1985-04-12',
+	user_id: doc.id
+	)
+
+
+
+org = User.create(
+	email: 'organizer1@mail.ru',
+	password: '123321',
+	role: :organizer
+	)
+
+Organizer.create(
+	company_name: 'КГМУ',
+	user_id: org.id
+	)
+
+
+oper = User.create(
+	email: 'operator1@mail.ru',
+	password: '123321',
+	role: :operator
+	)
+
+Operator.create(
+	first_name: 'Андрей',
+	last_name: 'Смирнов',
+	middle_name: 'Михайлович',
+	user_id: oper.id
+	)
+
+
