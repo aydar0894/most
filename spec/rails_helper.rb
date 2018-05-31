4cd295c5d2885380dc62319631ce5a0d86f47bee
Anvar <anvarzkr@gmail.com>
2018-05-31 14:39:14 +0300

    ! views

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,7 +37,8 @@ RSpec.configure do |config|
 
 
 
-  config.include Devise::Test::ControllerHelpers
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 
   # Use the following instead if you are on Devise <= 4.1.0
   # config.include Devise::TestHelpers, :type => :controller
